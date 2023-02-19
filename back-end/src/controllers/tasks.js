4a955314ac457c39ebf675e384272ca9b988012e
Anderson-Reges <andersonreges76@gmail.com>
2023-02-19 05:54:17 -0300

    Feat: creating layer controllers

--- back-end/src/controllers/tasks.js
+++ back-end/src/controllers/tasks.js
@@ -1,4 +1,4 @@
-const { getAllTasks, createTask, deleteTask } = require('../services/tasks');
+const { getAllTasks, createTask, deleteTask, getTaskWithYourProperties } = require('../services/tasks');
 
 const getAllTasksCont = async (_req, res) => {
   const allTasks = await getAllTasks();
@@ -8,6 +8,34 @@ const getAllTasksCont = async (_req, res) => {
   return res.status(200).json(allTasks);
 };
 
+const createNewTask = async (req, res) => {
+  const { taskName, shorDescription } = req.body;
+
+  const ifExistTask = await getTaskWithYourProperties(taskName, shorDescription);
+
+  if (ifExistTask.length !== 0) {
+    return res.status(409).json({ message: 'Existe uma tarefa com esse nome ou com essa descrição' });
+  }
+
+  await createTask(req.body);
+
+  const payload = {
+    taskName,
+    shorDescription,
+  };
+
+  return res.status(201).json(payload);
+};
+
+const deleteTaskControl = async (req, res) => {
+  const { id } = req.params
+
+  await deleteTask(id);
+  return res.status(204).json({ message: 'Task deletada com sucesso' });
+};
+
 module.exports = {
-  getAllTasksCont
+  getAllTasksCont,
+  createNewTask,
+  deleteTask,
 }
