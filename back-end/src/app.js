085e2c91eef496e2f04e4fb43adef9dba8b3bc4a
Anderson-Reges <andersonreges76@gmail.com>
2023-02-19 18:57:51 -0300

    Refactor: refactor fetch method and creating render task list

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const { getAllTasksCont, createNewTask, deleteOneTask } = require('./controllers/tasks');
+
+const corsOptions = {
+    origin:'http://localhost:3000', 
+    credentials:true, //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
 
 const app = express();
 
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app
   .get('/tasks', getAllTasksCont)
